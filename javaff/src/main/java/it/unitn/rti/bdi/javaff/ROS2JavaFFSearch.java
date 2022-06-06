a79bb973a31bfb7993a7ef528a22d85afa119b6b
Devis Dal Moro <devis.dalmoro@unitn.it>
2022-06-06 12:50:48 +0200

    Publishing in topic with a "PlanSys2 Plan" formatted msg

--- javaff/src/main/java/it/unitn/rti/bdi/javaff/ROS2JavaFFSearch.java
+++ javaff/src/main/java/it/unitn/rti/bdi/javaff/ROS2JavaFFSearch.java
@@ -5,21 +5,70 @@ import org.ros2.rcljava.publisher.Publisher;
 // import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.locks.ReentrantLock;
 import java.util.ArrayList;
+import java.util.StringTokenizer;  
 
 import javaff.JavaFF;
 import javaff.data.GroundProblem;
 import javaff.planning.TemporalMetricState;
 
+import plansys2_msgs.msg.Plan;
+import plansys2_msgs.msg.PlanItem;
+
 class SearchThread extends Thread{
 
   private boolean killMySelf = false;
   private SharedSearchData sharedSearchData;
-  private Publisher<example_interfaces.msg.String> planPublisher;
+  private Publisher<plansys2_msgs.msg.Plan> planPublisher;
 
-  public SearchThread(SharedSearchData sharedSearchData, Publisher<example_interfaces.msg.String> planPublisher){
+  public SearchThread(SharedSearchData sharedSearchData, Publisher<plansys2_msgs.msg.Plan> planPublisher){
     super();
     this.sharedSearchData = sharedSearchData;
     this.planPublisher = planPublisher;
+  }
+
+  /*
+   * Build from planString a Plansys2 Plan msg containing start time, action & duration for each action to be executed in the plan
+   * 
+   * planString has to contain line by line all actions to execute in this format "time: (action p1 p2 p3) [duration]"
+  */
+  private plansys2_msgs.msg.Plan buildPsys2Plan(String planString){
+    plansys2_msgs.msg.Plan psys2Plan = new plansys2_msgs.msg.Plan();
+    ArrayList<plansys2_msgs.msg.PlanItem> planItems = new ArrayList<>();
+
+    StringTokenizer st = new StringTokenizer(planString,"\n");  
+    while (st.hasMoreTokens()) { 
+        // build empty plansys2 plan item obj 
+        plansys2_msgs.msg.PlanItem planItem = new plansys2_msgs.msg.PlanItem();
+
+        // retrieve next line of the plan
+        String planItemString = st.nextToken();  
+        
+        // retrieve indexes for time, action, duration substrings
+        int endOfTimeIndex = planItemString.indexOf(":") - 1;
+
+        int startOfActionIndex = planItemString.indexOf("(");
+        int endOfActionIndex = planItemString.indexOf(")")+1;
+
+        int startDurationIndex = planItemString.indexOf("[")+1;
+        int endDurationIndex = planItemString.indexOf("]")-1;
+
+        // retrive actual action, duration, time data from plan line, converting them directly to correct type
+        float time =  Float.valueOf(planItemString.substring(0, endOfTimeIndex)).floatValue();
+        String action = planItemString.substring(startOfActionIndex, endOfActionIndex);
+        float duration = Float.valueOf(planItemString.substring(startDurationIndex, endDurationIndex)).floatValue();
+        // System.out.println("\""+time+"\"");
+        // System.out.println("\""+action+"\"");
+        // System.out.println("\""+duration+"\"");
+
+        // Set data to planItem and add it to items in plansys2 Plan.items msg
+        planItem.setTime(time);
+        planItem.setAction(action);
+        planItem.setDuration(duration);
+        planItems.add(planItem);
+    }  
+
+    psys2Plan.setItems(planItems);
+    return psys2Plan;
   }
 
   public void killMySelf(){killMySelf = true;}
@@ -36,8 +85,9 @@ class SearchThread extends Thread{
           //System.out.println("[Thread search]: Searching from " + this.sharedSearchData.initialState + " (open[0]=" + this.sharedSearchData.open.get(0) + ") ...");
           Thread.sleep(0);//searching phase
           String planString = JavaFF.plan(this.sharedSearchData.groundProblem, this.sharedSearchData.initialState);
-          example_interfaces.msg.String planMessage = new example_interfaces.msg.String();
-          planMessage.setData(planString);
+          System.out.println("\n\nplanString: \n" + planString);        
+
+          plansys2_msgs.msg.Plan planMessage = buildPsys2Plan(planString);
           this.planPublisher.publish(planMessage);
 
         this.sharedSearchData.searchLock.unlock();
@@ -55,7 +105,8 @@ class SearchThread extends Thread{
 
 class SharedSearchData{
   GroundProblem groundProblem;
-  TemporalMetricState initialState;//TODO just for skeleton
+  TemporalMetricState initialState;
+  
   ArrayList<String> open = new ArrayList<>();//TODO just for skeleton
   ReentrantLock searchLock = new ReentrantLock(true);
 }
@@ -69,7 +120,7 @@ public class ROS2JavaFFSearch extends ROS2JavaFF{
 
     private SharedSearchData sharedSearchData;
 
-    private Publisher<example_interfaces.msg.String> planPublisher;
+    private Publisher<plansys2_msgs.msg.Plan> planPublisher;
 
     // private AtomicBoolean killSearchThread = new AtomicBoolean(false);
 
@@ -78,7 +129,8 @@ public class ROS2JavaFFSearch extends ROS2JavaFF{
     public ROS2JavaFFSearch(String name, int i) {
       super(name, i);
       this.sharedSearchData = new SharedSearchData();
-      this.planPublisher = this.node.<example_interfaces.msg.String>createPublisher(example_interfaces.msg.String.class, "plan");
+      this.planPublisher = this.node.<plansys2_msgs.msg.Plan>createPublisher(plansys2_msgs.msg.Plan.class, "plan");
+
     }
 
     public OperationResult startSearch(String domain, String problem){
